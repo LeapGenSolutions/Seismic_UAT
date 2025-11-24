051717085a691a09ae1e234fb6729ad079a0b07e
LeapGenSolutions <website@leapgen.ai>
2025-11-23 23:42:16 -0500

    Update DOCTOR_PORTAL_URL for production environment

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const BACKEND_URL = "https://seismic-app-server-uat-gjdzdrewfmhscugq.canadacentral-01.azurewebsites.net/"
 // export const BACKEND_URL = "http://localhost:8080"
-export const DOCTOR_PORTAL_URL = "https://victorious-bay-02508b30f.2.azurestaticapps.net/"
+export const DOCTOR_PORTAL_URL = "https://vpmc-patient.seismicconnect.com/"
 export const SOS_URL = "https://seismicdockerbackend-uat-gyauhkasbcdzenbt.westus-01.azurewebsites.net/"
 export const STREAM_API_KEY = "qb4swzqqxr76"
 // export const REDIRECT_URI = "http://localhost:3000/"
