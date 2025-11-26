8b4b5c06ba1915760ea96cedff80926094f67f33
Anu Yammada <anuyammada@Anus-MacBook-Pro.local>
2025-11-26 13:39:54 -0500

    url changed for backend

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
-export const BACKEND_URL = "https://seismic-app-server-uat-gjdzdrewfmhscugq.canadacentral-01.azurewebsites.net/"
+export const BACKEND_URL = "https://seismic-app-server-uat-gjdzdrewfmhscugq.westus-01.azurewebsites.net/"
 // export const BACKEND_URL = "http://localhost:8080"
 export const DOCTOR_PORTAL_URL = "https://vpmc-patient.seismicconnect.com/"
 export const SOS_URL = "https://seismicdockerbackend-uat-gyauhkasbcdzenbt.westus-01.azurewebsites.net/"
 export const STREAM_API_KEY = "qb4swzqqxr76"
-// export const REDIRECT_URI = "http://localhost:3000/"
-export const REDIRECT_URI = "https://vpmc.seismicconnect.com"
+ export const REDIRECT_URI = "http://localhost:3000/"
+//export const REDIRECT_URI = "https://vpmc.seismicconnect.com"
 export const CHATBOT_URL = "https://sesmic-stg-frontend-c8fxgugwgpgsd9fg.centralus-01.azurewebsites.net/";
