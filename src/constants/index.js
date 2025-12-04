ce1a60168f4b8a9f679fafff5e7f8b254543cdd2
LeapGenSolutions <website@leapgen.ai>
2025-12-03 20:43:08 -0500

    Update CHATBOT_URL to new staging endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,4 +5,4 @@ export const SOS_URL = "https://seismicdockerbackend-uat-gyauhkasbcdzenbt.westus
 export const STREAM_API_KEY = "23dnbb2d3q8q"
  //export const REDIRECT_URI = "http://localhost:3000/"
 export const REDIRECT_URI = "https://vpmc.seismicconnect.com"
-export const CHATBOT_URL = "https://sesmic-stg-frontend-c8fxgugwgpgsd9fg.centralus-01.azurewebsites.net/";
+export const CHATBOT_URL = "https://sesmic-stg.azurewebsites.net/";
