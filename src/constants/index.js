5601c4c76611a9b570ae8568286d899bd5dc1738
LeapGenSolutions <website@leapgen.ai>
2026-02-11 02:35:36 -0500

    Update backend and portal URLs for production

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 export const BACKEND_URL = "https://seismic-app-server-uat-gjdzdrewfmhscugq.westus-01.azurewebsites.net/"
 // export const BACKEND_URL = "http://localhost:8080"
-export const DOCTOR_PORTAL_URL = "https://care-patient.seismicconnect.com/"
+export const DOCTOR_PORTAL_URL = "https://vpmc-patient.seismicconnect.com/"
 export const SOS_URL = "https://seismicdockerbackend-uat-gyauhkasbcdzenbt.westus-01.azurewebsites.net/"
 export const STREAM_API_KEY = "23dnbb2d3q8q"
  //export const REDIRECT_URI = "http://localhost:3000/"
-export const REDIRECT_URI = "https://care.seismicconnect.com"
+export const REDIRECT_URI = "https://vpmc.seismicconnect.com"
 export const CHATBOT_URL = "https://sesmic-stg.azurewebsites.net/";
