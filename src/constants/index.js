c244a5a98304bd5a88eca22b9493232d1db845ab
LeapGenSolutions <website@leapgen.ai>
2026-04-14 03:04:19 -0400

    Update backend and redirect URLs in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,9 +2,8 @@ export const BACKEND_URL = "https://seismic-backend-04272025-bjbxatgnadguabg9.ce
 // export const BACKEND_URL = "http://localhost:8080"
 export const DOCTOR_PORTAL_URL = "https://patient-dev.seismicconnect.com/"
 export const SOS_URL = "https://seismicdockerbackend-test-e0ducsgtggh7ftat.centralus-01.azurewebsites.net/"
-export const POWERBI_API_BASE_URL =
-  "https://seismicdockerbackend-fthfbxbscbcwe3hr.centralus-01.azurewebsites.net/"
+export const POWERBI_API_BASE_URL = "https://seismicdockerbackend-fthfbxbscbcwe3hr.centralus-01.azurewebsites.net/"
 export const STREAM_API_KEY = "72499ykcfb3z"
- export const REDIRECT_URI = "http://localhost:3000/"
-//export const REDIRECT_URI = "https://dev.seismicconnect.com/"
+//export const REDIRECT_URI = "http://localhost:3000/"
+export const REDIRECT_URI = "https://dev.seismicconnect.com/"
 export const CHATBOT_URL = "https://chat-strlit-gva3h7ekahgdctgk.centralus-01.azurewebsites.net";
